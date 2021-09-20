15d383adcbb08844ec677351b660f715817a83bf
giovanyrosales <gioer21@gmail.com>
2021-09-20 12:52:41 -0600

    acta_venta actualizada

--- resources/views/backend/reportes/acta_venta.blade.php
+++ resources/views/backend/reportes/acta_venta.blade.php
@@ -12,20 +12,21 @@ input {
 	width: 100%;
 }
 .table-head td{
-	font-family:Arial, sans-serif; font-size:14px;
+	font-family:Arial, sans-serif; font-size:13px;
 }
 .table-body td{
-	font-family:Arial, sans-serif; font-size:14px;
+	font-family:Arial, sans-serif; font-size:13px;
 }
+
 </style>
-<table style="height: 50px;" width="716">
+<table style="height: 10px;" width="716" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 705.023px;">&nbsp;</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 28px; width: 749.93px;" border="0">
+<table style="height: 17px; width: 749.93px;" border="0" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 364px;">&nbsp;</td>
@@ -33,7 +34,7 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 34px; width: 748.898px;" border="0">
+<table style="height: 28px; width: 748.898px;" border="0" cellspacing="0">
 <tbody>
 <tr style="height: 20.9375px;">
 <td style="width: 365px; height: 20.9375px;">&nbsp;</td>
@@ -41,7 +42,7 @@ input {
 </tr>
 </tbody>
 </table>
-<table border="0">
+<table border="0" cellspacing="0">
 <tbody>
 <tr>
 <td>&nbsp;</td>
@@ -49,7 +50,7 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="width: 36px; height: 41px;" border="0">
+<table style="width: 40px; height: 45px;" border="0" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 6px;">&nbsp;</td>
@@ -57,7 +58,7 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 25px; width: 750.07px;" border="0">
+<table style="height: 24px; width: 750.07px;" border="0" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 213px;">&nbsp;</td>
@@ -65,7 +66,7 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 26px; width: 754.125px;" border="0">
+<table style="height: 24px; width: 754.125px;" border="0" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 309px;">&nbsp;</td>
@@ -73,7 +74,7 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 27px; width: 753.461px;" border="0">
+<table style="height: 24px; width: 753.461px;" border="0" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 307px;">&nbsp;</td>
@@ -81,7 +82,7 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 29px; width: 750.703px;" border="0">
+<table style="height: 20px; width: 750.703px;" border="0" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 276px;">&nbsp;</td>
@@ -89,15 +90,15 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 24px; width: 749.711px;" border="0">
+<table style="height: 20px; width: 749.711px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 218px;">&nbsp;</td>
+<td style="width: 250px;">&nbsp;</td>
 <td style="width: 512.711px;">{{ $venta->nombreC }}</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 22px; width: 747.305px;" border="0">
+<table style="height: 18px; width: 747.305px;" border="0" cellspacing="0">
 <tbody>
 <tr>
 <td style="width: 314px;">&nbsp;</td>
@@ -105,10 +106,10 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 29px; width: 748.219px;" border="0">
+<table style="height: 18px; width: 748.219px;" border="0" cellspacing="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 323px;">&nbsp;</td>
+<td style="width: 275px;">&nbsp;</td>
 <td style="width: 118px;">{{ $venta->depaC }}</td>
 <td style="width: 33px;">&nbsp;</td>
 <td style="width: 72px; text-align: center;">{{ $venta->elo }}</td>
@@ -117,82 +118,82 @@ input {
 </tr>
 </tbody>
 </table>
-<table style="height: 29px; width: 753px;" border="0">
+<table style="height: 12px; width: 753px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 256px;">&nbsp;</td>
+<td style="width: 180px;">&nbsp;</td>
 <td style="width: 99px; text-align: center;">{{ $venta->expre }}</td>
-<td style="width: 126.766px;">&nbsp;</td>
-<td style="width: 236.234px;">&nbsp;</td>
+<td style="width: 126px;">&nbsp;</td>
+<td style="width: 236px;">&nbsp;</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 74px; width: 760px;" border="0">
+<table style="height: 70px; width: 760px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 233.016px;">&nbsp;</td>
-<td style="width: 509.531px;">{{ $venta->conti }}</td>
+<td style="width: 155px; margin-top: 0; padding-top: 0;">&nbsp;</td>
+<td style="width: 490px; margin-top: 0; padding-top: 0;">{{ $venta->conti }}</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 12px; width: 669px;" border="0">
+<table style="height: 35px; width: 669px; " border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 300px;">&nbsp;</td>
+<td style="width: 170px;">&nbsp;</td>
 <td style="width: 36px; text-align: center;">{{ $venta->semo2 }}</td>
 <td style="width: 61px;">&nbsp;</td>
 <td style="width: 26px; text-align: center;">-</td>
-<td style="width: 66.0859px;">&nbsp;</td>
-<td style="width: 28.9141px; text-align: center;">{{ $venta->herrado }}</td>
-<td style="width: 73px;">&nbsp;</td>
-<td style="width: 102px; text-align: center;">{{ $venta->vent }}</td>
+<td style="width: 66px;">&nbsp;</td>
+<td style="width: 28px; text-align: left;">{{ $venta->herrado }}</td>
+<td style="width: 45px;">&nbsp;</td>
+<td style="width: 55px; text-align: left;">{{ $venta->vent }}</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 5px; width: 749.117px;" border="0">
+<table style="height: 5px; width: 749.117px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 176px;">&nbsp;</td>
+<td style="width: 120px;">&nbsp;</td>
 <td style="width: 271px; text-align: center;">{{ $venta->fierro }}</td>
-<td style="width: 276.117px; text-align: center;">{{ $venta->numF }}</td>
+<td style="width: 250px; text-align: center;">{{ $venta->numF }}</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 22px; width: 747px;" border="0">
+<table style="height: 22px; width: 747px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 341.625px;">&nbsp;</td>
-<td style="width: 388.375px; text-align: center;">{{ $venta->depaF }}</td>
+<td style="width: 280px;">&nbsp;</td>
+<td style="width: 388px; text-align: center;">{{ $venta->depaF }}</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 138px; width: 724.922px;" border="0">
+<table style="height: 185px; width: 724.922px;" border="0" cellspacing="0">
 <tbody>
 <tr style="height: 142.422px;">
 <td style="width: 714.922px; height: 142.422px;">&nbsp;</td>
 </tr>
 </tbody>
 </table>
-<table style="width: 723px;" border="0">
+<table style="width: 723px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 296.266px;">&nbsp;</td>
+<td style="width: 10px;">&nbsp;</td>
 <td style="width: 405.734px; text-align: center;">&nbsp;{{ $venta->alcal }}</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 22px; width: 686.555px;" border="0">
+<table style="height: 22px; width: 686.555px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 373px; text-align: right;">{{ $venta->dia }}</td>
-<td style="width: 290.555px; text-align: center;">{{ $venta->mes }}</td>
+<td style="width: 130px; text-align: right;">{{ $venta->dia }}</td>
+<td style="width: 180px; text-align: center;">{{ $venta->mes }}</td>
 </tr>
 </tbody>
 </table>
-<table style="height: 8px; width: 485.961px;" border="0">
+<table style="height: 8px; width: 485.961px;" border="0" cellspacing="0">
 <tbody>
 <tr>
-<td style="width: 474.961px; text-align: right;">{{ $venta->ano }}</td>
+<td style="width: 10px; text-align: right;">{{ $venta->ano }}</td>
 </tr>
 </tbody>
 </table>
